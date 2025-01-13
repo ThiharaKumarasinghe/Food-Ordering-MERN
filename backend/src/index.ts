761c3266b5ad7160293a827fa6ef1f7c33a88d01
Thihara Kumarasinghe <thihara.kumarasinghe@gmail.com>
2025-01-13 06:06:46 +0530

    config mongodb

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,14 @@
 import express, {Request, Response} from 'express';
 import cors from 'cors';
 import "dotenv/config";
+import mongoose from 'mongoose';
 const PORT = 3000
 
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
+    console.log("MongoDB Database is Conected!")
+
+}
+)
 
 const app = express();
 app.use(express.json());
